180da114569f81e6a3a578110871dde16954e1bf
minoic <minoic2020@gmail.com>
2021-05-07 12:13:40 +0800

    LSTM(60,4) - 7 add loss history add positive/negative accuracy in history

--- lstm.py
+++ lstm.py
@@ -16,9 +16,10 @@ GOAL = 'V.csv'
 MODEL_NAME = 'DJI30'
 INPUT_LEN = 60
 OUTPUT_LEN = 7
-PREDICT_GROUPS = 20
+PREDICT_GROUPS = 100
 DIMS = ['Close', 'High', 'Low', 'Open']
 SCALER = Normalizer()
+Threshold = 0.04
 GAP = 0
 for i in range(INPUT_LEN, INPUT_LEN + OUTPUT_LEN):
     if i % OUTPUT_LEN == 0:
@@ -109,10 +110,13 @@ def convert_to_input(test_slice):
     return np.reshape(x_test, (1, INPUT_LEN, len(DIMS)))
 
 
+# 1 means rise
+# 0 means smooth
+# -1 means tumble
 def judge_pn(inc):
-    if inc >= 0.1:
+    if inc >= Threshold:
         return 1
-    elif 0.1 > inc > -0.1:
+    elif Threshold > inc > -Threshold:
         return 0
     else:
         return -1
@@ -121,15 +125,34 @@ def judge_pn(inc):
 def predict(model, test_data):
     if len(test_data) <= INPUT_LEN * 2:
         raise Exception('test data is too short to predict')
-    # create the datasets x_test and y_test
-    # cut_test_data = test_data[:-(len(test_data) % INPUTLEN)]
-    cut_test_data = test_data
     predictions = []
-    for i in range(INPUT_LEN, len(cut_test_data)):
+    for i in range(INPUT_LEN, len(test_data) - OUTPUT_LEN):
         if i % OUTPUT_LEN == OUTPUT_LEN - 1:
-            predictions.append(single_predict(model, cut_test_data[i - INPUT_LEN + 1:i + 1, :]))
+            predictions.append(single_predict(model, test_data[i - INPUT_LEN:i, :]))
     predictions = np.array(predictions)
-    return cut_test_data[:, 0], predictions
+    return test_data[:, 0], predictions
+
+
+def test_pn(model, test_data):
+    TP = 0.0
+    TN = 0.0
+    FP = 0.0
+    FN = 0.0
+    for i in range(INPUT_LEN, len(test_data) - OUTPUT_LEN):
+        pred = single_predict_pn(model, test_data[i - INPUT_LEN:i, :])
+        vald = judge_pn(test_data[i + OUTPUT_LEN][0] / test_data[i + 1][0] - 1.0)
+        if pred == 1 & vald == 1:
+            TP = TP + 1.0
+        if pred == 1 & vald == -1:
+            FP = FP + 1.0
+        if pred == -1 & vald == -1:
+            TN = TN + 1.0
+        if pred == -1 & vald == 1:
+            FN = FN + 1.0
+       # print(pred, ' ', vald)
+    A = (TP + TN) / (TP + TN + FP + FN)
+    print(A, TP, TN, FP, FN)
+    return A
 
 
 def single_predict(model, test_data):
@@ -160,18 +183,23 @@ def plot_figure(test_data, predictions):
     for i, pred in enumerate(predictions):
         padding = [None for _ in range(i * OUTPUT_LEN + GAP)]
         plt.plot(np.append(padding, pred), label='Prediction' + str(i))
-        # plt.legend()
     plt.savefig("predict.png")
     plt.show()
 
 
 def plot_history():
     plt.figure(figsize=(16, 8))
+    plt.subplot(2, 1, 1)
     j = json.loads(open('history.txt', 'r').read())
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.plot(j['loss'], label='loss')
     plt.plot(j['val_loss'], label='val_loss')
+    plt.legend()
+    plt.subplot(2, 1, 2)
+    plt.xlabel('epoch')
+    plt.ylabel('pn_accuracy')
+    plt.plot(j['pn_accuracy'], label='accuracy')
     plt.legend()
     plt.savefig('history.png')
     plt.show()
@@ -187,6 +215,7 @@ def load_test(filename):
 
 if __name__ == '__main__':
     model = build_model()
+    test_data = load_test(GOAL)
     try:
         model.load_weights(MODEL_NAME + '.h5')
     except Exception as E:
@@ -198,13 +227,16 @@ if __name__ == '__main__':
         np.save('dataset_x', x_train)
         np.save('dataset_y', y_train)
         print(x_train.shape, y_train.shape)
-        history = model.fit(x_train, y_train, batch_size=32, epochs=100, validation_split=0.3, shuffle=True, callbacks=[
-            # tf.keras.callbacks.EarlyStopping(mode='min', monitor='val_loss', patience=5, restore_best_weights=True)
+        pn_accuracy = []
+        history = model.fit(x_train, y_train, batch_size=32, epochs=30, validation_split=0.1, shuffle=True, callbacks=[
+            # tf.keras.callbacks.EarlyStopping(mode='min', monitor='loss', patience=5, restore_best_weights=True)
+            tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, logs: pn_accuracy.append(test_pn(model, test_data)))
         ])
         model.save_weights(MODEL_NAME + '.h5')
+        history.history['pn_accuracy'] = pn_accuracy
         with open('history.txt', 'wt') as f:
             json.dump(history.history, f, indent=4, separators=(',', ': '))
         plot_history()
-    test_data = load_test(GOAL)
     cut_test_data, predictions = predict(model, test_data)
     plot_figure(cut_test_data, predictions)
+    print(test_pn(model, test_data))
